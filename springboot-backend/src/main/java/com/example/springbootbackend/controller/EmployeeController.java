cabe2e1f78d4553b40cf42294d252f56bd0af47e
Yeon <dusrksl@gmail.com>
2022-10-22 00:25:43 -0400

    added customized exception and employee controller

---
+++ springboot-backend/src/main/java/com/example/springbootbackend/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package com.example.springbootbackend.controller;
+
+import com.example.springbootbackend.model.Employee;
+import com.example.springbootbackend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/")
+public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    // get all employees
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
