8bf31af95f04938ba264c32f582a63924bf6c169
Yeon <dusrksl@gmail.com>
2022-10-23 23:48:25 -0400

    added header and footer

--- react-springboot-crud-app/react-frontend/src/App.js
+++ react-springboot-crud-app/react-frontend/src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
-import ListEmployeeComponent from './components/ListEmployeeComponent';
+import "./App.css";
+import FooterComponent from "./components/FooterComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import ListEmployeeComponent from "./components/ListEmployeeComponent";
 
 function App() {
   return (
-    <div className="container">
-      <ListEmployeeComponent />
+    <div>
+      <HeaderComponent />
+      <div className="container">
+        <ListEmployeeComponent />
+      </div>
+      <FooterComponent />
     </div>
   );
 }
