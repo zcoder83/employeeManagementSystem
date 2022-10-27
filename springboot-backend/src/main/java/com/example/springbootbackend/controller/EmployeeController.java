087e6909160ce50c709b8fefb081f7ba9be37662
Yeon <dusrksl@gmail.com>
2022-10-26 21:17:55 -0400

    created delete rest api

--- springboot-backend/src/main/java/com/example/springbootbackend/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/example/springbootbackend/controller/EmployeeController.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("api/v1/")
@@ -41,5 +44,15 @@ public class EmployeeController {
         Employee updatedEmployee = employeeRepository.save(employee);
 
         return ResponseEntity.ok(updatedEmployee);
+    }
+
+    // delete employee rest api
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:" + id));
+        employeeRepository.delete(employee);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
     }
 }
